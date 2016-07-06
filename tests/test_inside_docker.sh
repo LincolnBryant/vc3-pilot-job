0e6a87189dabda6c987c95af83ffbb19b45dfdc2
Lincoln Bryant <lincolnb@hep.uchicago.edu>
2016-07-06 09:38:40 -0500

    added test scripts

---
+++ tests/test_inside_docker.sh
@@ -0,0 +1,13 @@
+#!/bin/sh -xe
+
+OS_VERSION=$1
+CVMFS_EXTERNAL_URL=$2
+
+# Clean the yum cache
+yum -y clean all
+yum -y clean expire-cache
+
+# First, install all the needed packages.
+rpm -Uvh https://dl.fedoraproject.org/pub/epel/epel-release-latest-${OS_VERSION}.noarch.rpm
+
+yum install gcc
